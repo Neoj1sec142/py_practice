bc7186f2c1704772c6d29a5d37967357bdba6f66
Mark Harmon <neothej1sec@gmail.com>
2022-05-16 21:45:51 -0500

    completed lesson the range function

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # py_practice
 Practice python lessons
+* This read me will serve as a todo list to move forward with python 
+    from basic string and number manipulation on...
+    * Starting off with lists and loops we will progress through my learning
+    into more complex code with extra frameworks...
+    * Wish me Luck
+
+## Todo Next:
+[] Loops
+[] Lists 
+[] Read-Only Lists (tuples)
+[] Key Value Pairs
+[] Sets
+[] Functions
+[] Supermarket Cashier Project
+[] File Handling
+[] Exception Handling
+[] Modules
+[] OOP
+[] Multithreading
+[] Library Management Project 2
+[] Database Connectivity
+[] GUI
+[] Employee CRUD App
+[] Python Interview
---
+++ one.py
@@ -0,0 +1,28 @@
+# Loops: 
+###############################
+# 2 types of iteration: 
+# Definite: the number of repitition is specified explicitly in advance
+# Indefinite: the code block executes until some condition is met
+# 
+# Different Loops:
+# For: used for defitine iteration, that is when the number of iterations is known
+# While: used for indefinite iteration, number of iterations is unknown
+# 
+# Range Function:
+# * the range() is abuilt in function in python used when a user need to perform 
+#   an action for a specific number of times, used to generate a sequence of numbers 
+#   and is commonly used with the for loop
+#   * the range function takes mainly 3 arguments start end and step
+# start: an integer starting from which the sequence of integers is to be returned
+# stop: an integer before which the sequence of integers is to be returned. 
+#       The range of integers ends at stop -1
+# step(optional):it is an integer value which determines the increment in the sequence
+
+# ex:
+print(list(range(5))) #single argument
+#signifies the end of the range (excluded)
+print(list(range(1,5))) # two arguments
+# signifies start and end
+print(list(range(1, 10,2))) # three args
+#signifies the start, stop, ad steps taken to get there
+print(list(range(70,100,5))) #= [70,75,80,85,90,95]
