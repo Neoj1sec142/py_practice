6950ab53ef5a78180a25a78571c64bd8f3836da6
Mark Harmon <neothej1sec@gmail.com>
2022-05-18 11:13:51 -0500

    started lessons on lists

---
+++ lists.py
@@ -0,0 +1,9 @@
+## Lists:
+########################
+# lists are ordered collections of multiple values
+# lists are mutable, meaning values of lists can e changed 
+#       or manipulated over a period of time
+# lists allow if to store duplicate values
+# list1 = []
+# list2 = [1,2,3,4,5,6]
+# list3 = ['Jogn', 23, 5.6, True]
