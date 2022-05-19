189f308e2c373c54e344b32e5bb03d228bdc58d4
Mark Harmon <neothej1sec@gmail.com>
2022-05-19 00:15:38 -0500

    finished lessons on manipulating lists and operations on lists

--- lists.py
+++ lists.py
@@ -7,3 +7,37 @@
 # list1 = []
 # list2 = [1,2,3,4,5,6]
 # list3 = ['Jogn', 23, 5.6, True]
+
+# List manipulation:
+#   Accessing elements of a list
+#   List Slicing
+#   updating a list
+#   Dleting elements of a list
+
+# names = ["John", "Sam", "Barry", "Lin"]
+# print(names)
+# print(names[0]) #= john
+# print(names[3]) #= lin
+# print(names[1:3]) #= sam, lin
+# print(names[2:]) #= barry, lin
+
+# marks = [56, 76, 69, 71, 39]
+# marks[4] = 43
+# print(marks)
+
+# names = ["John", "Sam", "Barry", "Lin"]
+# del names[1] #= removed sam
+# print(names)
+
+# Concatenation, Repetition, Membership
+
+# lst1 = [1,2,3]
+# lst2 = [4,5,6]
+# print(lst1+lst2) #= [1, 2, 3, 4, 5, 6] concatentation
+# print(lst1*3) #= [1, 2, 3, 1, 2, 3, 1, 2, 3] repetition
+
+# enrolledList = ["sam", 'mike', 'kane', 'nick']
+# print("sam" in enrolledList) #= True
+# print("samuel" in enrolledList) #= False
+
+
