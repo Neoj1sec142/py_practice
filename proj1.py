743ea4056c21bb3647a2092eba38cfbeb47f6acf
Mark Harmon <neothej1sec@gmail.com>
2022-05-20 18:06:55 -0500

    set up function for get discount

--- proj1.py
+++ proj1.py
@@ -31,3 +31,21 @@ def getPrice(prod, amt):
     print(prod + '$' + str(priceData[prod]) + 'x' +
     str(amt) + '=' + str(subtotal))
     return subtotal
+
+def getDiscount(billAmt, membership):
+    discount = 0
+    if billAmt >= 25:
+        if membership == 'Gold':
+            billAmt = billAmt * 0.80
+            discount = 20
+        elif membership == 'Silver':
+            billAmt = billAmt * 0.90
+            discount = 10
+        elif membership == 'Bronze':
+            billAmt = billAmt * 0.95
+            discount = 5
+        print(str(discount) + '% off for' + membership + '' + 'membership on total amount: $' + str(billAmt))
+    else: 
+        print("No discount on items under 25")
+    return billAmt
+getDiscount(30, 'Silver')
