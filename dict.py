e24b3894eb947b7cbbd7e4d4074947372480f8ff
Mark Harmon <neothej1sec@gmail.com>
2022-05-19 21:22:00 -0500

    finished manipulating dictionaries lesson

--- dict.py
+++ dict.py
@@ -12,3 +12,20 @@
 # "Nick":["Java", "JavaScript"]
 # }
 # print(studentsEnrolled)
+
+# Manipulating Dictionaries:
+#   Dictionarys in python are unordered so we cannot use the concept of
+#   an index to access and manipulate elements of a dictionary
+#   instead we use the concept of keys since those are unique
+#   so the values can be uniquely identified with the keys
+# 
+#  Keys can be used either insdie [] or with the get() method
+#  [] returns a key error if key not found
+#  get() returns none if key not found
+# example = {1:"John", 2:"Nick"}
+# example[1]= "Den"
+# example[3]= "Sim"
+# print(example) #= {1: 'Den', 2: 'Nick', 3: 'Sim'}
+# if the key value pair is present then it gets updated if not it gets created
+# del example[1] #= we can delete KVP by key or the whole dictionary with del
+#  #
