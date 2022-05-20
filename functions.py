36c77973bde63826c1f41f19e3c8c47492a0e44e
Mark Harmon <neothej1sec@gmail.com>
2022-05-20 11:43:08 -0500

    finished undertanding the structure lesson

---
+++ functions.py
@@ -0,0 +1,29 @@
+## FUNCTIONS ## 
+#######################
+# In simple words functions help us to groups tasks that only get
+# executed when they are called -- a block of code that performs 
+# a particular task and may accept optional user input and 
+# accordingly give outputs
+# 
+# creating functions allows us to reuse the code later on without having
+# to re create the function we can just call it instead
+# 
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter another number: "))
+# num3 = num1+num2
+# print("The sum is: ",num3)
+# Functions SYNTAX:
+# def DoSomething(param1, param2):
+#   value = 1
+#   return value
+# 
+# In python the def keyword is used to define a function
+# The def keyword is followed by the name of the function and ()
+# Keep your names smart
+# () may contain the optional input paramters. a : marks the end of the fun header
+# 
+# Once the function header id defined, we can define the funciton body that may
+# contian one or more Python statements -- indented
+# return statments are option but return the value from function
+# calling a funciton EX func_name()
+#  #
