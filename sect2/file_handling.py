f34520acdae4f14091c573a8616c5d8a57d90453
Mark Harmon <neothej1sec@gmail.com>
2022-05-20 22:22:14 -0500

    finished openeing files lesson

---
+++ sect2/file_handling.py
@@ -0,0 +1,36 @@
+## FILE HANDLING ##
+#############################
+# Files are named locations on a disk to store related info
+# They are used to permanently store data in a non-volatile
+#   memory such as hard diskd and more
+# 
+# These files can be .csv .txt and more
+# 
+# In simple words a file is a named location to store information
+# One can just flush data into files such as .csv .txt .docx etc
+# 
+# Opening Files:
+# open() funciton is a built in way to open files creating a file object 
+# syntax:
+# file_object = open(file_name, access_mode)
+#   file_name - is the name of the file with extension. we can also add the absolute path
+#   access_mode - it defines the mode in which the file has been opened. 
+# 
+# Access Modes:
+#   Read Mode:
+        # r -opens a file in readonly mode file pointer is placed at the beginning of the file
+        # rb - opens a file for reading only in binary format
+        # r+ - opens a file for both reading and writing
+        # rb+ - opens a file for both reading and writing in binary format #
+#   Write Mode:
+        # w - opens a file in write-only mode, it will overwrite the file if the file exists.
+        #           if no file exists it will creat a new one
+        # wb - opens a file for write only in binary
+        # w+ - opens a file for both reading and writing
+        # wb+ - opens a file for reading and writing in binary format
+#   Append Mode:
+       # a - opens a file for appending and adds the appendage at the end
+       # ab - opens a file for appending in binary format
+       # a+ - opens the file for appending and reading
+       # ab+ - opens the file for appending and reading in binary format
+#    #
