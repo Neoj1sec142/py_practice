ec759067b6b165d645de879d807b6e5000887bbd
Mark Harmon <neothej1sec@gmail.com>
2022-05-21 11:38:39 -0500

    finished reading data from files lesson

--- sect2/file_handling.py
+++ sect2/file_handling.py
@@ -33,4 +33,18 @@
        # ab - opens a file for appending in binary format
        # a+ - opens the file for appending and reading
        # ab+ - opens the file for appending and reading in binary format
-#    #
+## below is the correct syntax for opening a file
+# fp = open('python.txt', 'r')
+
+# Python has a few built in methods to read the content of the file.
+# read() - reads a string from an open file. It read the content until it reaches the end of the file
+# read(size) - accepts a param as size, the passed param is the num of bytes to be read from the opened file
+#                this method starts reading from the beginning of the file until the num of byte specified
+# readline() - It reads on single line of the file at a time
+# readline() - It reads all the lines until it reaches the end of the file
+# fp = open('python.txt', 'r')
+# print(fp.read(6)) #= Python
+# print(fp.readline()) #= Python is a general-purpose, high-level, object-oriented programming language.
+
+## Reading Data From Files:
+#  #
