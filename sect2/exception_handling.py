0c77bf1789b28dc49ebc82a19a9f908812a33212
Mark Harmon <neothej1sec@gmail.com>
2022-05-21 17:27:21 -0500

    finished intro to exception handling lesson

---
+++ sect2/exception_handling.py
@@ -0,0 +1,10 @@
+## EXCEPTION HANDLING ##
+# As a user enters inforamtion to a db and say they leave a spot blank
+#   this could arise DB Errors and possibly stop the program
+#   Exception handling is the process of handling these errors to avoid that situtation
+# Error Types:
+#   Syntax Err - error in the source code of a program/also called compilion errors
+#   Logic Err - undesirable actions caused by inproper logic in code
+#   Runtime Err - means program complied correctly but when running leads 
+#           to abnormal behavior or terminate the program
+#  #
