e33937a8c227887453335e584e37b78ebde4783a
Mark Harmon <neothej1sec@gmail.com>
2022-05-21 23:36:50 -0500

    finished exception handling lesson

--- sect2/exception_handling.py
+++ sect2/exception_handling.py
@@ -7,4 +7,20 @@
 #   Logic Err - undesirable actions caused by inproper logic in code
 #   Runtime Err - means program complied correctly but when running leads 
 #           to abnormal behavior or terminate the program
-#  #
+# Runtime Errors are generates when the program is runnning and lead to abnormal
+#   behavoir or termination of the program -- such are called EXCEPIONS
+# 
+# Examples:
+#   division by zero
+#   accessing a file that does not exist
+#   addition of two incompatible types
+#   trying to access a nonexistent index of a sequence
+#   removing the table fro the disconnected database server
+# 
+# Exception handling allows you ro separate error-handling from normal code
+# an exception is a python object which represents and error
+# allows you to simulate consequenses as the error handling takes place
+#   in onw place and in one manner
+# an exception is a convenient method for handling err messages
+# 
+# Oython Exceptions: #
