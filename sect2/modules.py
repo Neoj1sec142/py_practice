bd1551d741b0573963caae3b98a5265a5edf121b
Mark Harmon <neothej1sec@gmail.com>
2022-05-22 21:54:19 -0500

    finished types of modules lesson

---
+++ sect2/modules.py
@@ -0,0 +1,35 @@
+## Modules ##
+# Reusability is an important part of coding
+# There are situations where we might opt to resuse a particular code rather
+#       than develooping something from scratch ... enter modules
+# 
+# Modules refer to a file containing Python statements and definitions.
+#   These are the pre-written codes that are written by someone else
+#   which you can directly use in your code
+# 
+# We can define our most used functions in a module and import it, 
+#   instead of copying their definitions into different programs
+# 
+#### Types of Modules: ####
+#   Built-in Modules
+#   Third-Party Modules
+#   Custom Modules
+
+# Built In Modules:
+#   -are the modules built into python and maintained by the python dev team
+#   -to use you can just import them and make the call to the methods you want
+#       *EX: Math, Random, Time, Socket etc
+
+# Third-Party Modules:
+#   -only different here is we need to install them first (can be installed using pip)
+#   - these modules are uploaded and stored at PyPI repo. The Python Package Index,
+#           is the official software repo for Python
+#       *Install CMD: 
+#           windows - pip install module_name
+#           mac/linux - pip3 install module_name
+
+# Custom Modules:
+#   -created by the dev(me) for reuse of code (need to service modules)
+
+# How to use a Module:
+#  #
