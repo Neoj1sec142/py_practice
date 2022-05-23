b1715fdb9793f26fbe41111c0c78a2963d8c4baa
Mark Harmon <neothej1sec@gmail.com>
2022-05-23 02:05:50 -0500

    started codewars again

---
+++ codewars.py
@@ -0,0 +1,6 @@
+## CodeWars Python:
+
+# This is my answer capitalize all words in a string with contractions
+# def to_jaden_case(string):
+#     res = ' '.join(elem.capitalize() for elem in string.split())
+#     return res
