c0ba5955fa54b3e67dc1090ea834441a398e649f
Mark Harmon <neothej1sec@gmail.com>
2022-05-25 16:50:06 -0500

    finished intro to OOP lessons

---
+++ sect2/OOP.py
@@ -0,0 +1,45 @@
+## Object Oriented Programming (OOP) ##
+
+# In the subject "Getting Started" we defined Python
+#   as a general-purpose, high-level, object-oriented
+#   programming language...
+# 
+# OOP:
+# In programming, there are different paradigms or 
+#   patterns that define how to write and structure our code.
+#   Also, each paradigm has different functionalicties and behaviors
+# EX of vairous programming patterns:
+#   Structural programming
+#   Procedural Programming
+#   OOP
+#   Functional Programming
+#   etc
+# 
+# OOP is a programming paradigm that provides a means 
+#   for structuring programs so that properties and behaviors
+#   are bundled into individual objects
+# In the initial subjects, we have seen that the type() method
+#   in Python return the type of object i.e the data type of 
+#   variable
+
+#   EX: 
+# name = "John"
+# print(type(name))
+
+# Here focus on the word, 'class'. The variable 'name' belongs 
+#   to something called <class 'string'>
+# 
+# Classes and Objects:
+# OOP is a programming paradigm that provides a means for structuring
+#   programs so that properties and behaviors are bundled into 
+#   individual objects
+# We can say the object of the class 'string' at the most basic lvl
+# OOPs in Python is a programming approach that focuses on using 
+#   objects and classes as the building blocks of the programs
+# It allows devs to develop apps using OOPs approach with a major focus
+#   on code reusablility
+# In simple words OOP is an approach for modeling concrete, real-world
+#   thing, and relations between those things
+# OOP models real-world entities as programming objects that have 
+#   some data associated with them and can perform certain functions
+#  #
