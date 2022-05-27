5180118cf95b79b49e345a7dabfd0142b61fd7c1
Mark Harmon <neothej1sec@gmail.com>
2022-05-26 21:59:02 -0500

    finished foudational theroy lessons of multithreading

--- sect2/multithreading.py
+++ sect2/multithreading.py
@@ -1,4 +1,18 @@
 ## MULTI-THREADING: ##
 # While humans multitask computers multithread
 # multithreading is the process by which computers erpform various
-#   tasks simultaneously #
+#   tasks simultaneously
+# 
+# Multiple Tasks: (Building the Foundation)
+# Each task on the computer is handled by a process. 
+#   Each process is handled by something called a thread
+# Thus, at the most basic level, a thread is responsible for 
+#   executing the various tasks on the computer
+# Task -> Process -> Thread
+# Process: is basically the program in exectuion. 
+#   When you start an application on your computer, 
+#   the operating system creates a process
+# in simple words a process is an instance of a computer 
+#   program the is being executed
+# Thread: is an entity within a process that can be scheduled for execution. 
+#   Also it is the smallest unit of processing that can be performed in and Operating System #
