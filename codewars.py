be4541d28c26f82195782a16f324cda01974770d
Mark Harmon <neothej1sec@gmail.com>
2022-05-29 10:47:40 -0500

    finished 7 kyu codewars python

--- codewars.py
+++ codewars.py
@@ -4,3 +4,13 @@
 # def to_jaden_case(string):
 #     res = ' '.join(elem.capitalize() for elem in string.split())
 #     return res
+
+# my answer to your a square (7 kyu)
+#Checks to see if any given integer is a perfect square
+# import math
+# def is_square(n):
+#     if n>=0 and math.sqrt(n) == math.floor(math.sqrt(n)):
+#         return True
+#     else:
+#         return False
+            
