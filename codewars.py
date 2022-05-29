e461cce99358ef028b331909b98696ab8cabab2d
Mark Harmon <neothej1sec@gmail.com>
2022-05-29 14:24:58 -0500

    touched name of kata

--- codewars.py
+++ codewars.py
@@ -14,7 +14,7 @@
 #     else:
 #         return False
 
-# my answer to build with cubes (6 kyu)
+# my answer to Build a pile of Cubes (6 kyu)
 #Your task is to construct a building which will be a pile of n cubes. 
 #   The cube at the bottom will have a volume of n^3, the cube above will 
 #   have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
