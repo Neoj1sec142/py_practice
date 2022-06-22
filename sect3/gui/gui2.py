33e0370356052923a1ede3f6eb28aedd0a44e12a
Mark Harmon <neothej1sec@gmail.com>
2022-06-22 14:20:06 -0500

    finished lessons on working with widgets

---
+++ sect3/gui/gui2.py
@@ -0,0 +1,39 @@
+# how to add widgets to a frame container ex
+# from tkinter import *
+# window = Tk()
+# frame = Frame(window)
+# lbl = Label(frame, text='Inside the frame')
+# btn = Button(frame, text='Inside the frame')
+# frame.pack()
+# lbl.pack(side=TOP)
+# btn.pack(side=BOTTOM)
+# window.mainloop()
+
+# checkbutton example
+# from tkinter import *
+# window = Tk()
+# lbl = Label(window, text="Choose your favorite programming language: ")
+# frame = Frame(window)
+# python = Checkbutton(frame, text='Python')
+# java = Checkbutton(frame, text='Java')
+# js = Checkbutton(frame, text='JavaScript')
+# cpp = Checkbutton(frame, text='C++')
+# lbl.pack()
+# frame.pack()
+# python.pack()
+# java.pack()
+# js.pack()
+# cpp.pack()
+# window.mainloop()
+
+# radiobutton example
+from tkinter import *
+window = Tk()
+lbl = Label(window, text='Gender:')
+var = StringVar()
+male = Radiobutton(window, text='Male', variable=var, value='M')
+female = Radiobutton(window, text='Female', variable=var, value='F')
+lbl.pack()
+male.pack()
+female.pack()
+window.mainloop()
