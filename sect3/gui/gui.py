03fb6e5fb08d3af8bad4078b1e28c0248f8d4f79
Mark Harmon <neothej1sec@gmail.com>
2022-06-22 15:45:59 -0500

    finished chapter on GUIs 100% on test

--- sect3/gui/gui.py
+++ sect3/gui/gui.py
@@ -1,23 +1,67 @@
 # First GUI Window
+# from tkinter import *
+# from PIL import Image, ImageTk
+# from tkinter import messagebox
+
+
+# # USING the Command Option
+# def gui():
+#     window = Tk()
+
+#     name = Label(window, text="PythonX - Learn Python", bg='white', fg='Blue', font=("Serif", 16))
+#     img = Image.open('python.png')
+#     logo = ImageTk.PhotoImage(img)
+#     image = Label(window, image=logo)
+#     frame = Frame(window)
+#     username = Label(frame, text='Username', font=('Serif', 12))
+#     inputBox = Entry(frame)
+#     button = Button(window, text='Lets Start', command=showMessage)
+#     button.bind('<Button-1>', greetUser)
+#     greet = Label(window)
+
+#     name.pack()
+#     image.pack()
+#     frame.pack()
+#     username.pack(side=LEFT)
+#     inputBox.pack(side=RIGHT)
+#     button.pack(side=BOTTOM)
+#     greet.pack()
+#     window.mainloop()
+
+# def showMessage():
+#     messagebox.showinfo("PythonX - Learn Python", 'Welcome')
+# if __name__ == '__main__':
+#     gui()
+
+# Using the BIND function
 from tkinter import *
-from PIL import Image, ImageTk
-window = Tk()
+from PIL import ImageTk, Image
+from tkinter import messagebox
 
-img = Image.open('python.png')
-logo = ImageTk.PhotoImage(img)
-image = Label(window, image=logo)
+def greetUser():
+    username = inputBox.get()
+    greet['text'] = 'Welcome' + username
+    window = Tk()
+    name = Label(window, text="PythonX - GUI with Bind", bg='white', fg='Blue', font=('Serif', 16))
+    img = Image.open('python.png')
+    logo = ImageTk.PhotoImage(img)
+    image = Label(window, image=logo)
+    frame = Frame(window)
+    username = Label(frame, text='Username', font=('Serif', 12))
+    inputBox = Entry(frame)
+    button = Button(window, text='Lets Start')
+    button.bind('<Button>', greetUser)
+    greet = Label(window)
+    name.pack()
+    image.pack()
+    frame.pack() 
+    username.pack(side=LEFT)
+    inputBox.pack(side=RIGHT)
+    button.pack()
+    greet.pack()
+    window.mainloop()
 
-button = Button(window, text='Lets Start')
-
-username = Label(window, text='Username', font=('Serif', 12))
-inputBox = Entry(window)
-
-name = Label(window, text="PythonX - Learn Python", bg='white', fg='Blue', font=("Serif", 16))
-name.pack()
-image.pack()
-username.pack(side=LEFT)
-inputBox.pack(side=RIGHT)
-button.place(x=100, y=180)
-
-window.mainloop()
-
+# We have added our GUI code into a function called gui() just to format the code better,
+# the gui() is then called in the main section in the special variable name
+# we have simply defined a method called showMessage() that shows a message box
+# finally, in the button, we add the command option and pass the name of the handler method as value #
