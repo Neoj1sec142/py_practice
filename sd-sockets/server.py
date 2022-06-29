81a3050de6452bf16d0de33beeff38a8a6b88d70
Mark Harmon <neothej1sec@gmail.com>
2022-06-28 21:32:21 -0500

    finished lesson on maintianing a connection streamed with updated data

--- sd-sockets/server.py
+++ sd-sockets/server.py
@@ -1,4 +1,8 @@
 import socket
+import time
+
+HEADERSIZE = 10
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -7,5 +11,13 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established")
-    clientsocket.send(bytes("Welcome to the server", "utf-8"))
-    clientsocket.close()
+
+    msg = 'Welcome to the server'
+    msg = f"{len(msg):<{HEADERSIZE}}" + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
+    # while True:
+    #     time.sleep(2)
+    #     msg = f'the time is {time.time()}'
+    #     msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    #     clientsocket.send(bytes(msg, "utf-8"))
