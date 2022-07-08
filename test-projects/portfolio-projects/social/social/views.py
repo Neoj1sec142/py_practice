a0f0b09a743154b9c0badd2d90e9a11237eab3b7
Mark Harmon <neothej1sec@gmail.com>
2022-07-07 23:55:49 -0500

    working on social app

--- test-projects/portfolio-projects/social/social/views.py
+++ test-projects/portfolio-projects/social/social/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class UserPostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
