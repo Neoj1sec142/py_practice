4822f76e1d66b0764064866e6d40d26007518e74
Mark Harmon <neothej1sec@gmail.com>
2022-07-08 17:35:39 -0500

    started front end set up

---
+++ test-projects/portfolio-projects/meretry/client/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import './styles/App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+      </header>
+      <Routes>
+        <Route path='/' element={}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
