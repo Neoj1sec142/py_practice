5edc59858d568e58e179fa0b177965d56cf08737
Mark Harmon <neothej1sec@gmail.com>
2022-07-09 11:47:53 -0500

    set up basic context and login structure

--- test-projects/portfolio-projects/meretry/client/src/App.js
+++ test-projects/portfolio-projects/meretry/client/src/App.js
@@ -1,17 +1,48 @@
+// Service and Module Imports
+import React, {useState, useEffect} from 'react'
 import { Route, Routes } from 'react-router-dom'
-import LoginContext from './services/LoginContext.jsx'
+import { CheckSession } from "./service/Auth";
+import {LoginContext} from './services/LoginContext.jsx'
+// Page and Component Imports
+import Home from './pages/Home'
+// Style Imports
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/App.css'
 
 const App = () => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+  const [userInfo, setUserInfo] = useState(null)
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    setUserInfo(user)
+    toggleAuthenticated(true)
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    setUserInfo(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if(token){
+      checkToken()
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-        
-      </header>
+      <header className="App-header"></header>
+      <LoginContext.Provider value={{userInfo, setUserInfo}}>
       <Routes>
-        <Route path='/' element={}/>
+        <Route path='/' element={<Home/>}/>
       </Routes>
+      </LoginContext.Provider>
     </div>
   )
 }
