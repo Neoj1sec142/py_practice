76112cf0aaac65944d0ba6bbbba1b73e652c0dfb
Mark Harmon <neothej1sec@gmail.com>
2022-07-10 00:37:51 -0500

    finished modules and std library

--- csPy/StLibrary/intro.py
+++ csPy/StLibrary/intro.py
@@ -1,11 +1,34 @@
 #import main as mn
-import sys
-sys.path.append('/Users/neo/.mine/practice/python/py_practice/mymodules')
-from quick1 import find_target as fi
+# import sys
+# sys.path.append('/Users/neo/.mine/practice/python/py_practice/mymodules')
+# from quick1 import find_target as fi
 
 # from main import find_index as fi, test as t
-courses = ['Python', 'Java', 'PHP', 'Perl', 'JS']
+# courses = ['Python', 'Java', 'PHP', 'Perl', 'JS']
 
-print(fi(courses, 'PHP'))
+# print(fi(courses, 'PHP'))
 # print(t)
 # print(sys.path) #= prints paths python searches for modules
+
+# import random as ran
+
+courses = ['Python', 'Java', 'PHP', 'Perl', 'JS']
+# rand_course = ran.choice(courses)
+
+# print(rand_course)
+
+# import math as m
+# rads = m.radians(90)
+# print(rads)
+
+# import datetime as dt
+# import calendar as c
+
+# today = dt.date.today()
+# print(today)
+# print(c.isleap(2020))
+
+import antigravity
+# print(os.getcwd()) #= prints current working dir
+
+# print(os.__file__) #= prints the path of the whole os library
