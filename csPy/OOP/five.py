64cbec31a1f57328a853b3420587fd82cb53d159
Mark Harmon <neothej1sec@gmail.com>
2022-07-09 22:26:16 -0500

    finished oop pt 5 dunder methods

--- csPy/OOP/five.py
+++ csPy/OOP/five.py
@@ -1,1 +1,40 @@
 ## Special and Dunder Methods OOP pt5
+
+class Employee:
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+        # Employee.num_of_emps += 1
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+    def __repr__(self):
+        return "Employee('{}', {}, {})".format(self.first, self.last, self.pay)
+    def __str__(self):
+        return'{} - {}'.format(self.fullname(), self.email)
+    def __add__(self, other):
+        return self.pay + other.pay
+    def __len__(self):
+        return len(self.fullname())
+
+emp_1 = Employee('Neo', 'Skele', 50000)
+emp_2 = Employee('Skid', 'Row', 60000)
+
+print(len(emp_1))
+
+# print(len(str('test'))) #= 4
+# print('test'.__len__()) #= 4
+# print(emp_1 + emp_2) #= 110000
+# print(1+2)
+# print('a' + 'b')
+# repr(emp_1)
+# str(emp_1)
+# print(emp_1.__repr__())
+# print(emp_1.__str__())
+# print(int.__add__(1,2)) #=3
+
