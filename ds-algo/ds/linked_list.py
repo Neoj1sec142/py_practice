d9e76a3396759ec6a8adef2346d874b6b9ad69aa
Mark Harmon <neothej1sec@gmail.com>
2022-07-20 14:47:00 -0500

    working with linked lists

--- ds-algo/ds/linked_list.py
+++ ds-algo/ds/linked_list.py
@@ -7,3 +7,38 @@ class Node:
         self.data = data
     def __repr__(self):
         return "<Node data: %s>"%self.data
+
+class LinkedList:
+    '''Singly Linked List'''
+    def __init__(self):
+        self.head = None
+    def is_empty(self):
+        return self.head == None
+    def size(self):
+        '''returns number of nodes in list with linear time'''
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next_node
+        return count
+    def add(self, data):
+        '''adds a new node containing data at the head of the list
+            this option takes constant time which is best case'''
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def __repr__(self):
+        '''returns a str representation of the list O(n) time'''
+        nodes = []
+        current = self.head
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]"%current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]"%current.data)
+            else:
+                nodes.append("[%s]"%current.data)
+            current = current.next_node
+        return '-> '.join(nodes)
