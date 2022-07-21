4053c74e3d7f3daa3df81f38b1eacb3f8a1e116b
Mark Harmon <neothej1sec@gmail.com>
2022-07-20 22:22:06 -0500

    finished quick sort

--- ds-algo/quick_sort.py
+++ ds-algo/quick_sort.py
@@ -7,9 +7,13 @@ def quicksort(values):
     less_than_pivot = []
     greater_than_pivot = []
     pivot = values[0]
-    for value in values:
-        if value in values[1:]:
+    for value in values[1:]:
+        if value <= pivot:
             less_than_pivot.append(value)
+        else:
+            greater_than_pivot.append(value)
+    print("%15s %1s %-15s"%(less_than_pivot, pivot, greater_than_pivot))
+    return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
 
 sorted_numbers = quicksort(nums)
 print(sorted_numbers)
